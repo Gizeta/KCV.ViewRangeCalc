356dc9fb767b69a4868f359d1571023abd377826
Gizeta <0w0@gizeta.tk>
2014-12-28 16:41:28 +0900

    初次提交。

---
+++ KCV.ViewRangeCalc/KanColleWrapperHelper.cs
@@ -0,0 +1,24 @@
+﻿using Grabacr07.KanColleWrapper.Models;
+using System;
+using System.Reflection;
+
+namespace Gizeta.KCV.ViewRangeCalc
+{
+    public static class RawDataWrapperHelper
+    {
+        public static T GetRawData<T>(this RawDataWrapper<T> source) where T : class
+        {
+            Type type = typeof(RawDataWrapper<T>);
+            return type.GetProperty("RawData", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).GetValue(source) as T;
+        }
+    }
+
+    public static class FleetHelper
+    {
+        public static void SetTotalViewRange(this Fleet fleet, int value)
+        {
+            Type type = typeof(Fleet);
+            type.GetProperty("TotalViewRange", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).SetValue(fleet, value);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+KCV.ViewRangeCalc
+====================
+KanColleViewer索敌值计算插件
+
+使用方法
+-------
+KCV.ViewRangeCalc.dll放入KanColleViewer的Plugin目录中
+
+截图
+----
+![image1](https://raw.github.com/Gizeta/KCV.ViewRangeCalc/master/ScreenShots/screenshot1.jpg)
+
+使用库
+-----
+* [KanColleViewer](http://grabacr.net/kancolleviewer) - 提督業も忙しい！
+* [Livet](https://github.com/ugaya40/Livet) - WPF MVVM Infrastructure
+* [MetroRadiance](https://github.com/Grabacr07/MetroRadiance) - MetroLikeWindow
+* [FiddlerCore](http://www.telerik.com/fiddler/fiddlercore)
+* [Rx](http://msdn.microsoft.com/en-us/data/gg577609) - The Reactive Extensions
+
+协议
+---
+使用MIT开源协议发布。
